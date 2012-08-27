d8b214a77be954c8e06e7554a92bdafcb6ab7e91
Tuncay Senturk <tuncay.senturk@gmail.com>
2012-08-27 23:25:23 +0300

    https://ecosystem.atlassian.net/browse/AMKT-2788
    Signed-off-by: Tuncay Senturk <tuncay.senturk@gmail.com>

--- src/main/java/com/tuncaysenturk/jira/plugins/compatibility/servlet/ConfigureServlet.java
+++ src/main/java/com/tuncaysenturk/jira/plugins/compatibility/servlet/ConfigureServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
 import com.atlassian.jira.ComponentManager;
